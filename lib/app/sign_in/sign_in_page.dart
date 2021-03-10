e3eeaf06f2bccf30967063d4d84a1fd9d689c1a0
Cesc Nguyen <thuocnv1802@gmail.com>
2021-03-10 15:22:42 +0700

    reuse widget buttion

---
+++ lib/app/sign_in/sign_in_page.dart
@@ -0,0 +1,85 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_app/app/sign_in/sign_in_button.dart';
+import 'package:flutter_app/services/auth.dart';
+
+class SignInPage extends StatelessWidget {
+  final void Function(User) onSignIn;
+  final AuthBase auth;
+
+  SignInPage({Key key , @required this.auth, @required this.onSignIn}): super(key: key);
+
+  void _signInAnonymously() async {
+    try {
+      final user = await auth.signInAnonymously();
+      onSignIn(user);
+    } catch (e) {
+      print(e.toString());
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Time Tracker'),
+        elevation: 2.0,
+      ),
+      body: _buildContent(),
+      backgroundColor: Colors.grey,
+    );
+  }
+
+  Widget _buildContent() {
+    return Padding(
+      padding: EdgeInsets.all(16.0),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Text(
+            'Sign in',
+            textAlign: TextAlign.center,
+            style: TextStyle(fontSize: 32.0, fontWeight: FontWeight.w600),
+          ),
+          SizedBox(height: 48.0),
+          SignInButton(
+            text: 'Sign in with Google',
+            color: Colors.white,
+            textColor: Colors.black87,
+            onPressed: () {},
+          ),
+          SizedBox(height: 8.0),
+          SignInButton(
+            text: 'Sign in with Facebook',
+            color: Color(0xFF334D92),
+            textColor: Colors.white,
+            onPressed: () {},
+          ),
+          SizedBox(height: 8.0),
+          SignInButton(
+            text: 'Sign in with Email',
+            color: Colors.teal[700],
+            textColor: Colors.white,
+            onPressed: () {},
+          ),
+          SizedBox(height: 8.0),
+          Text(
+            '-- or --',
+            style: TextStyle(fontSize: 14.0, color: Colors.black87),
+            textAlign: TextAlign.center,
+          ),
+          SizedBox(
+            height: 8.0,
+          ),
+          SignInButton(
+            text: 'Go anonymous',
+            color: Colors.lime[300],
+            textColor: Colors.white,
+            onPressed: _signInAnonymously,
+          ),
+        ],
+      ),
+    );
+  }
+}
