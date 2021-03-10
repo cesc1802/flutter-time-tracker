b37c1ec079cc2d586411b4bd0b20b7149615d14d
Cesc Nguyen <thuocnv1802@gmail.com>
2021-03-10 15:24:07 +0700

    splash screen

---
+++ lib/app/landing_page.dart
@@ -0,0 +1,46 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_app/app/home_page.dart';
+import 'package:flutter_app/app/sign_in/sign_in_page.dart';
+import 'package:flutter_app/services/auth.dart';
+
+class LandingPage extends StatefulWidget {
+  const LandingPage({Key key, @required this.auth}) : super(key: key);
+  final AuthBase auth;
+
+  @override
+  _LandingPageState createState() => _LandingPageState();
+}
+
+class _LandingPageState extends State<LandingPage> {
+  User _user;
+
+  @override
+  void initState() {
+    super.initState();
+    widget.auth.authStateChanges().listen((user) {
+      print('uid: ${user?.uid}');
+    });
+    _updateUser(widget.auth.currentUser);
+  }
+
+  void _updateUser(User user) {
+    setState(() {
+      _user = user;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    if (_user == null) {
+      return SignInPage(
+        onSignIn: (user) => _updateUser(user),
+        auth: widget.auth,
+      );
+    }
+    return HomePage(
+      onSignOut: () => _updateUser(null),
+      auth: widget.auth,
+    );
+  }
+}
