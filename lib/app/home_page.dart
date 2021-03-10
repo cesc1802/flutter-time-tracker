577936e7f9031e2bf3e413c334b47d6a437e0891
Cesc Nguyen <thuocnv1802@gmail.com>
2021-03-10 15:23:33 +0700

    splash screen

---
+++ lib/app/home_page.dart
@@ -0,0 +1,36 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_app/services/auth.dart';
+
+class HomePage extends StatelessWidget {
+  HomePage({Key key, @required this.auth, @required this.onSignOut})
+      : super(key: key);
+  final VoidCallback onSignOut;
+  final AuthBase auth;
+
+  Future<void> _signOut() async {
+    try {
+      await auth.signOut();
+      onSignOut();
+    } catch (e) {
+      print(e.toString());
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Home Page'),
+        actions: [
+          FlatButton(
+              onPressed: _signOut,
+              child: Text(
+                'Logout',
+                style: TextStyle(fontSize: 18.0, color: Colors.white),
+              ))
+        ],
+      ),
+    );
+  }
+}
